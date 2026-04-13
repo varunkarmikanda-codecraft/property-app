969e671fc366d17305f030ae674194c486f944cd
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-04-13 10:16:15 +0000

    implemented Home component and integrate into root application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  // template: `<h1>Hello {{ title() }}</h1>`,
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('property-app');
+  // protected readonly title = 'property-app';
+
+  // check if the setTimeout, event listener is fired, or a promise got resolved/rejected, network call
+
+  ngOnInit() {
+    console.log('App component initialized');
+    // this.title.set('property app reloaded')
+  }
 }
