cc24c67ac2eec793375e8b577ab81aefd671caef
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-04-16 11:17:55 +0000

    Enhance location details component with navigation buttons and update routing logic

--- src/app/components/location-details/location-details.ts
+++ src/app/components/location-details/location-details.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LocationService } from '../../services/location-service';
 import { HousingLocationInfo } from '../../models/housing-location-info';
 
@@ -19,17 +19,29 @@ export class LocationDetails {
   locationService: LocationService = inject(LocationService);
 
   location: HousingLocationInfo | undefined;
+
+  router = inject(Router);
   
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    // this.housingLocationId = Number(this.route.snapshot.params['id']);
     
-    console.log("ID of the location: ",this.housingLocationId)
+    // console.log("ID of the location: ",this.housingLocationId)
 
-    this.location = this.locationService.getLocationForId(this.housingLocationId)
+    // this.location = this.locationService.getLocationForId(this.housingLocationId)
+
+    // LocationDetails.count++;
+    // console.log("Number of instances: ", LocationDetails.count)
+    
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.housingLocationId = Number(params['id']);
+      this.location = this.locationService.getLocationForId(this.housingLocationId);
+    });
 
     LocationDetails.count++;
-    console.log("Number of instances: ", LocationDetails.count)
-    
+    console.log("Number of instances: ", LocationDetails.count);
   }
 
   ngAfterViewInit() {
@@ -39,6 +51,28 @@ export class LocationDetails {
   ngOnDestroy() {
     console.log("On Destroyed")
     LocationDetails.count--;
+  }
+
+  handlePrev(housingLocationInfo: HousingLocationInfo | undefined) {
+    if(housingLocationInfo && housingLocationInfo.id >= 0) {
+      const prevId = housingLocationInfo.id - 1;
+      if(prevId >= 0) {
+        this.router.navigate(['details', prevId])
+        console.log("details/",prevId)
+      }
+    }
+    console.log(housingLocationInfo)
+  }
+
+  handleNext(housingLocationInfo: HousingLocationInfo | undefined) {
+    if(housingLocationInfo) {
+      const nextId = housingLocationInfo.id + 1;
+      if(nextId < this.locationService.getAllLocation().length) {
+        this.router.navigate(['details', nextId])
+        console.log("details/", nextId)
+      }
+    }
+    console.log(housingLocationInfo)
   }
 
   static count = 0;
