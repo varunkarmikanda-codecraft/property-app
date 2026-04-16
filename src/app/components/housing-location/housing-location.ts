2f93bd2c9c16a774fe2e3733c8f6a9218d9261f8
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-04-16 06:21:11 +0000

    mock service injection demonstration

--- src/app/components/housing-location/housing-location.ts
+++ src/app/components/housing-location/housing-location.ts
@@ -1,28 +1,31 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { HousingLocationInfo } from '../../models/housing-location-info';
+import { LocationService } from '../../services/location-service';
+import { MockLocationService } from '../../services/mock-location.service';
 
 @Component({
   selector: 'app-housing-location',
   imports: [],
   templateUrl: './housing-location.html',
   styleUrl: './housing-location.css',
+  providers: [{ provide: LocationService, useClass: MockLocationService }],
 })
 export class HousingLocation {
-
   housingLocation = input.required<HousingLocationInfo>();
   onLocationClick = output<HousingLocationInfo>();
+  locationService = inject(LocationService);
 
-  showWifi = false
+  showWifi = false;
 
   handleClick(event: MouseEvent) {
-    if(this.housingLocation().wifi) {
-      this.showWifi = !this.showWifi
+    if (this.housingLocation().wifi) {
+      this.showWifi = !this.showWifi;
     }
 
-    console.log(event.target)
+    console.log(event.target);
 
-    console.log(`${this.housingLocation().name} is clicked`)
-    this.onLocationClick.emit(this.housingLocation())
+    console.log(`${this.housingLocation().name} is clicked`);
+    this.onLocationClick.emit(this.housingLocation());
   }
 
   // ngOnInit(){
---
+++ src/app/services/mock-location.service.ts
@@ -0,0 +1,105 @@
+import { Injectable } from '@angular/core';
+import { HousingLocationInfo } from '../models/housing-location-info';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MockLocationService {
+
+  static numberOfInstances = 0;
+
+  constructor() {
+    MockLocationService.numberOfInstances += 1;
+    console.log("Number of instances of ", MockLocationService.numberOfInstances)
+  }
+
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  private readonly housingLocationList: HousingLocationInfo[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
+      availableUnits: 5,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+  ];
+
+
+  getAllLocation() {
+    return this.housingLocationList;
+  }
+}
