4e8a1aaff33735a980b05653b4f7ef0840d5a335
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-04-23 07:22:31 +0000

    reactive forms

--- src/app/components/reactive-form/reactive-form.ts
+++ src/app/components/reactive-form/reactive-form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -10,7 +10,21 @@ import {FormControl, ReactiveFormsModule} from '@angular/forms';
 export class ReactiveForm {
   name = new FormControl('');
 
+  myProfileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  })
+
   updateName() {
     this.name.setValue('Varun');
+  }
+
+  handleChange(event: Event) {
+    // console.log(this.name.value)
+    console.log((event.target as HTMLInputElement).value)
+  }
+
+  onSubmit() {
+    console.warn(this.myProfileForm.value)
   }
 }
